f5fe895f7cbc3ffa3c6e697209058033930488cc
KhushalVashisth <139761210+KhushalVashisth@users.noreply.github.com>
2025-02-06 10:56:37 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 5173, 
-    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': 'http://localhost:4000',
     },
   },
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom', 'react-toastify','react-toastify/dist/ReactToastify.css','axios'], // ✅ Add react-toastify
-    },
-  },
 });
+
